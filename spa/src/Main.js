c5e536879fd266950abecd51542e223b911f46ea
Tyler Inari <github@al.tyler.jp>
2023-01-12 02:01:22 -0800

    worl

--- spa/src/Main.js
+++ spa/src/Main.js
@@ -1,6 +1,11 @@
 import { ReactDOM } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Register from "./pages/Register";
+import Nopage from "./pages/Nopage";
 
 function Main() {
 	return (
