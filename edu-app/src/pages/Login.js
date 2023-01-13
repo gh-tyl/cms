4d765a2849a8a5acaff9f03bd430ec1fc87fd76e
Tyler Inari <github@al.tyler.jp>
2023-01-13 12:30:45 -0800

    done 2023-01-11

--- edu-app/src/pages/Login.js
+++ edu-app/src/pages/Login.js
@@ -1,42 +1,44 @@
-import {useState} from "react";
+import { useState } from "react";
 import userSrv from "../Services/userSrv";
 import cryptoJs from "crypto-js";
 
-function Login(){
-    const [res,setRes] = useState('');
-    const enc = (data,key) =>{
-      const encData = cryptoJs.AES.encrypt(data,key).toString();
-      return encData;
-    };
-    const dec = (encData,key) =>{
-        const decData = cryptoJs.AES.decrypt(encData,key);
-        return decData.toString(cryptoJs.enc.Utf8);
-    };
-    const login = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        // formData.append("fakeInput","fakevalue");
-        userSrv.register("login.php",formData)
-        .then(response=>{
-          setRes(response.data);
-          if(response.data === "Valid User"){
-            sessionStorage.setItem("user", enc(formData.get("email"), "Matheus"));
-            console.log(dec(sessionStorage.getItem("user"), "Matheus"));
-          }
-        })
-        .catch(err=>{
-          console.log(err);
-        })
-      }
-    return(
-        <>
-            <h1>{res}</h1>
-            <form onSubmit={login}>
-                <input type="email" name="email" placeholder="email"/>
-                <input type="password" name="pass" placeholder="pass"/>
-                <button type="submit">LOGIN</button>
-            </form>
-        </>
-    );
+function Login() {
+  const [res, setRes] = useState('');
+  const enc = (data, key) => {
+    const encData = cryptoJs.AES.encrypt(data, key).toString();
+    return encData;
+  };
+  const dec = (encData, key) => {
+    const decData = cryptoJs.AES.decrypt(encData, key);
+    return decData.toString(cryptoJs.enc.Utf8);
+  };
+  const login = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    // formData.append("fakeInput","fakevalue");
+    userSrv.register("login.php", formData)
+      .then(response => {
+        if (response.data === "Valid User") {
+          console.log(formData.get("email"));
+          sessionStorage.setItem("user", enc(formData.get("email"), "Matheus"));
+          console.log(dec(sessionStorage.getItem("user"), "Matheus"));
+        } else {
+          console.log("Invalid User");
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  return (
+    <>
+      <h1>{res}</h1>
+      <form onSubmit={login}>
+        <input type="email" name="email" placeholder="email" />
+        <input type="password" name="pass" placeholder="pass" />
+        <button type="submit">LOGIN</button>
+      </form>
+    </>
+  );
 }
 export default Login;
